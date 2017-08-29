180ef4acf8fac44a7e3292e5ffdcc65e6f9e1ebd
¨gaurhari¨ <gaurharidass@gmail.com>
2017-08-29 11:50:28 +0100

    interface added for read function

--- src/main/java/uk/ac/ebi/ddi/service/db/service/similarity/ICitationService.java
+++ src/main/java/uk/ac/ebi/ddi/service/db/service/similarity/ICitationService.java
@@ -1,8 +1,11 @@
 package uk.ac.ebi.ddi.service.db.service.similarity;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.repository.Query;
 import uk.ac.ebi.ddi.service.db.model.similarity.Citations;
 import uk.ac.ebi.ddi.service.db.repo.similarity.ICitationRepo;
+
+import java.util.List;
 
 /**
  * Created by gaur on 20/07/17.
@@ -10,4 +13,7 @@ import uk.ac.ebi.ddi.service.db.repo.similarity.ICitationRepo;
 public interface ICitationService {
 
     void saveCitation(Citations citations);
+
+
+    Citations read(String accession,String database);
 }
