eff52510731ec7033ad3aaa4f84a4fe15e6a4a68
¨gaurhari¨ <gaurharidass@gmail.com>
2017-08-29 11:35:31 +0100

    added citation repo to retrieve citation counts

--- src/main/java/uk/ac/ebi/ddi/service/db/repo/similarity/ICitationRepo.java
+++ src/main/java/uk/ac/ebi/ddi/service/db/repo/similarity/ICitationRepo.java
@@ -2,11 +2,17 @@ package uk.ac.ebi.ddi.service.db.repo.similarity;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import uk.ac.ebi.ddi.service.db.model.similarity.Citations;
 import uk.ac.ebi.ddi.service.db.model.similarity.DatasetStatInfo;
+
+import java.util.List;
 
 /**
  * Created by gaur on 20/07/17.
  */
 public interface ICitationRepo extends MongoRepository<Citations,ObjectId> {
+
+    @Query("{'$and': [{'accession': ?0},{'database': ?1}]}")
+    Citations readCitationCount(String accession, String database);
 }
