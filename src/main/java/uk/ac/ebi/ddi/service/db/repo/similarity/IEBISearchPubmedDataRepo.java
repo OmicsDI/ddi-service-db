1010cfa835831b44ec0bacfb6f270ad6c9588586
¨gaurhari¨ <gaurharidass@gmail.com>
2017-08-30 13:38:07 +0100

    added new query for ebi search pubmed count

--- src/main/java/uk/ac/ebi/ddi/service/db/repo/similarity/IEBISearchPubmedDataRepo.java
+++ src/main/java/uk/ac/ebi/ddi/service/db/repo/similarity/IEBISearchPubmedDataRepo.java
@@ -2,10 +2,16 @@ package uk.ac.ebi.ddi.service.db.repo.similarity;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import uk.ac.ebi.ddi.service.db.model.similarity.EBISearchPubmedCount;
+import uk.ac.ebi.ddi.service.db.model.similarity.ReanalysisData;
 
 /**
  * Created by gaur on 27/07/17.
  */
 public interface IEBISearchPubmedDataRepo extends MongoRepository<EBISearchPubmedCount,ObjectId> {
+
+    @Query("{'$and': [{'accession': ?0},{'database': ?1}]}")
+    EBISearchPubmedCount getSearchCount(String accession, String database);
+
 }
