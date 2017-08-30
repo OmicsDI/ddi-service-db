0b904d49e050c0b89344c7245f8ff8edbe05ec5f
¨gaurhari¨ <gaurharidass@gmail.com>
2017-08-30 13:25:08 +0100

    added for reanalysis count on data

--- src/main/java/uk/ac/ebi/ddi/service/db/repo/similarity/IReanalysisRepo.java
+++ src/main/java/uk/ac/ebi/ddi/service/db/repo/similarity/IReanalysisRepo.java
@@ -2,10 +2,14 @@ package uk.ac.ebi.ddi.service.db.repo.similarity;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import uk.ac.ebi.ddi.service.db.model.similarity.ReanalysisData;
 
 /**
  * Created by gaur on 27/07/17.
  */
 public interface IReanalysisRepo extends MongoRepository<ReanalysisData,ObjectId> {
+
+    @Query("{'$and': [{'accession': ?0},{'database': ?1}]}")
+    ReanalysisData getReanalysisCount(String accession, String database);
 }
