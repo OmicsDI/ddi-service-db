dca38b655e090ee880c9d8d36512af305474f74a
¨gaurhari¨ <gaurharidass@gmail.com>
2017-08-30 14:45:46 +0100

    updated service

--- src/main/java/uk/ac/ebi/ddi/service/db/service/similarity/EBIPubmedSearchService.java
+++ src/main/java/uk/ac/ebi/ddi/service/db/service/similarity/EBIPubmedSearchService.java
@@ -16,7 +16,7 @@ public class EBIPubmedSearchService implements IEBIPubmedSearchService{
         ebiSearchPubmedRepo.save(ebiSearchPubmedCount);
     }
 
-    public EBISearchPubmedCount getSearchCount(String accession,String database){
-        return ebiSearchPubmedRepo.getSearchCount(accession,database);
+    public EBISearchPubmedCount getSearchCount(String accession){
+        return ebiSearchPubmedRepo.getSearchCount(accession);
     }
 }
