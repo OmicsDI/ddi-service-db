cf39457b9266b1831110252bd941ba188b744ade
Andrey Zorin <azorin@ebi.ac.uk>
2017-09-02 16:09:45 +0100

    spring app refactor: move repos to ddi-service-db, remove new bean declarations

---
+++ src/main/java/uk/ac/ebi/ddi/service/db/repo/database/DatabaseDetailRepository.java
@@ -0,0 +1,17 @@
+package uk.ac.ebi.ddi.service.db.repo.database;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+import uk.ac.ebi.ddi.service.db.model.database.DatabaseDetail;
+
+/**
+ * Created by root on 16.05.17.
+ */
+@Repository
+public interface DatabaseDetailRepository extends PagingAndSortingRepository<DatabaseDetail,String>{
+
+
+    @Query("{databaseName : ?0}")
+    DatabaseDetail findDatabaseByName(String databaseName);
+}
