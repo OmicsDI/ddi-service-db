264f682e13ffe9d473ec063cc8d34194cf81ba46
¨gaurhari¨ <gaurharidass@gmail.com>
2017-09-10 19:17:13 +0100

    added reanalysis service and reanalysis count
    Signed-off-by: ¨gaurhari¨ <¨gaurharidass@gmail.com¨>

--- src/main/java/uk/ac/ebi/ddi/service/db/service/similarity/ReanalysisDataService.java
+++ src/main/java/uk/ac/ebi/ddi/service/db/service/similarity/ReanalysisDataService.java
@@ -2,10 +2,13 @@ package uk.ac.ebi.ddi.service.db.service.similarity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uk.ac.ebi.ddi.service.db.model.dataset.Dataset;
+import uk.ac.ebi.ddi.service.db.model.dataset.Scores;
 import uk.ac.ebi.ddi.service.db.model.similarity.Citations;
 import uk.ac.ebi.ddi.service.db.model.similarity.ReanalysisData;
 import uk.ac.ebi.ddi.service.db.repo.similarity.ICitationRepo;
 import uk.ac.ebi.ddi.service.db.repo.similarity.IReanalysisRepo;
+import uk.ac.ebi.ddi.service.db.service.dataset.DatasetService;
 
 /**
  * Created by gaur on 27/07/17.
@@ -16,11 +19,27 @@ public class ReanalysisDataService implements IReanalysisDataService {
     @Autowired
     public IReanalysisRepo reanalysisRepo;
 
+    @Autowired
+    private DatasetService datasetService;
+
     public void saveReanalysis(ReanalysisData reanalysisData){
+        Dataset dataset = datasetService.read(reanalysisData.getAccession(),reanalysisData.getDatabase());
+        if (dataset != null) {
+            if(dataset.getScores() != null) {
+                dataset.getScores().setReanalysisCount(reanalysisData.getTotal());
+            }else{
+                Scores scores = new Scores();
+                scores.setReanalysisCount(reanalysisData.getTotal());
+                dataset.setScores(scores);
+            }
+            datasetService.update(dataset.getId(),dataset);
+        }
         reanalysisRepo.save(reanalysisData);
     }
 
     public ReanalysisData getReanalysisCount(String accession,String database){
         return reanalysisRepo.getReanalysisCount(accession,database);
     }
+
+
 }
