d3d96c0052c4c995336747b6363f3a296dcef777
¨gaurhari¨ <gaurharidass@gmail.com>
2017-09-11 14:10:19 +0100

    added database constants to service db as well

--- src/main/java/uk/ac/ebi/ddi/service/db/utils/Constants.java
+++ src/main/java/uk/ac/ebi/ddi/service/db/utils/Constants.java
@@ -16,5 +16,60 @@ public class Constants {
     public static String ACCESSION_FIELD = "accession";
     public static String TOTAL_FIELD ="total";
 
+    public enum Database{
+        PRIDE("Pride", "pride"),
+        PEPTIDEATLAS("PeptideAtlas", "peptide_atlas"),
+        MASSIVE("Massive", "massive"),
+        METABOLIGHTS("MetaboLights", "metabolights_dataset"),
+        EGA("EGA", "ega"),
+        GPMDB("GPMDB",  "gpmdb"),
+        GNPS("GNPS", "gnps"),
+        ARRAY_EXPRESS("ArrayExpress", "arrayexpress-repository"),
+        METABOLOMEEXPRESS("MetabolomeExpress", "metabolome_express"),
+        EXPRESSION_ATLAS("ExpressionAtlas", "atlas-experiments"),
+        METABOLOMICSWORKBENCH("MetabolomicsWorkbench", "metabolomics_workbench"),
+        BIOMODELS("BioModels Database","biomodels"),
+        LINCS("LINCS","lincs"),
+        PAXDB("PAXDB","paxdb"),
+        JPOST("JPOST Repository","jpost"),
+        EVA("EVA","eva");
 
+        String databaseName;
+        String solarName;
+
+        Database(String databaseName, String solrName) {
+            this.databaseName = databaseName;
+            this.solarName = solrName;
+        }
+
+        public String getDatabaseName() {
+            return databaseName;
+        }
+
+        public void setDatabaseName(String databaseName) {
+            this.databaseName = databaseName;
+        }
+
+        public String getSolarName() {
+            return solarName;
+        }
+
+        public void setSolarName(String solarName) {
+            this.solarName = solarName;
+        }
+
+        public static String retriveAnchorName(String name){
+            for(Database database: values())
+                if(database.solarName.equalsIgnoreCase(name))
+                    return database.getDatabaseName();
+            return name;
+        }
+
+        public static String retriveSorlName(String name) {
+            for(Database database: values())
+                if(database.getDatabaseName().equalsIgnoreCase(name))
+                    return database.getSolarName();
+            return name;
+        }
+    }
 }
