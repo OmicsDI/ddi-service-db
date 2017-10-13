09ad5466e0c49149cfa48cd80a5cdbd825ddaccb
¨gaurhari¨ <gaurharidass@gmail.com>
2017-10-13 17:01:45 +0100

    updated for logger collection string

--- src/main/java/uk/ac/ebi/ddi/service/db/utils/Constants.java
+++ src/main/java/uk/ac/ebi/ddi/service/db/utils/Constants.java
@@ -18,6 +18,7 @@ public class Constants {
     public static String TOTAL_FIELD ="total";
     public static String REANALYSIS_FIELD = "reanalysis_count";
     public static String VIEWCOUNT_FIELD = "view_count";
+    public static String LOGGER_COLLECTION = "logger.event";
 
     public enum Database{
         PRIDE("Pride", "pride"),
