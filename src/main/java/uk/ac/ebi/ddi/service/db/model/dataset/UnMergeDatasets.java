bcea23aff66c5d08199b7e084f624733578697c2
Gaurhari <gaurharidass@gmail.com>
2018-06-25 09:54:51 +0100

    added unmerged datsets fille

---
+++ src/main/java/uk/ac/ebi/ddi/service/db/model/dataset/UnMergeDatasets.java
@@ -0,0 +1,49 @@
+package uk.ac.ebi.ddi.service.db.model.dataset;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Component;
+
+
+@Document(collection = "unmerged")
+public class UnMergeDatasets{
+
+    private String masterAccession;
+
+    private String masterDatabase;
+
+    private MergedDataset data;
+
+    public String getMasterAccession() {
+        return masterAccession;
+    }
+
+    public void setMasterAccession(String masterAccession) {
+        this.masterAccession = masterAccession;
+    }
+
+    public MergedDataset getDataset() {
+        return data;
+    }
+
+    public void setDataset(MergedDataset dataset) {
+        this.data = dataset;
+    }
+
+    public String getMasterDatabase() {
+        return masterDatabase;
+    }
+
+    public void setMasterDatabase(String masterDatabase) {
+        this.masterDatabase = masterDatabase;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+                "masterAccession='" + masterAccession + '\'' +
+                ", masterDatabase='" + masterDatabase + '\'' +
+
+                '}';
+    }
+}
