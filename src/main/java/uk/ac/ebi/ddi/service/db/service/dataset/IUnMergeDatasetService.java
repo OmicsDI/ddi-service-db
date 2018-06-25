0d6482bdd3d0365c01be6f49afeedb1bca50935d
Gaurhari <gaurharidass@gmail.com>
2018-06-25 09:56:21 +0100

    unmerged dataset service

---
+++ src/main/java/uk/ac/ebi/ddi/service/db/service/dataset/IUnMergeDatasetService.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.ddi.service.db.service.dataset;
+
+import uk.ac.ebi.ddi.service.db.model.dataset.UnMergeDatasets;
+
+import java.util.List;
+
+public interface IUnMergeDatasetService {
+
+    public void save(UnMergeDatasets unMergeDatasets);
+
+    public List<UnMergeDatasets> findAll();
+}
