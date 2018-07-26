9d631f06a6517c5e0872b4f53a349f3d4cc2d800
Gaurhari <gaurharidass@gmail.com>
2018-07-26 12:14:34 +0100

    updated unmerge datasets repository

--- src/main/java/uk/ac/ebi/ddi/service/db/repo/dataset/IUnMergeDatasetsRepo.java
+++ src/main/java/uk/ac/ebi/ddi/service/db/repo/dataset/IUnMergeDatasetsRepo.java
@@ -2,12 +2,17 @@ package uk.ac.ebi.ddi.service.db.repo.dataset;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import uk.ac.ebi.ddi.service.db.model.dataset.Dataset;
+import uk.ac.ebi.ddi.service.db.model.dataset.MergedDataset;
 import uk.ac.ebi.ddi.service.db.model.dataset.UnMergeDatasets;
 
 @Repository
 public interface IUnMergeDatasetsRepo extends MongoRepository<UnMergeDatasets,ObjectId> {
+
+    @Query(value = "{'$and':[{data.accession : ?0}, {data.database : ?1}]}", delete = true)
+    Long deleteByDataset(String accession, String database);
 
 
 }
