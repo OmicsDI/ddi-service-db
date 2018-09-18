08267ac877c70c1dcda1ccddca15b36b6d5ab7a8
Gaurhari <gaurharidass@gmail.com>
2018-09-18 14:18:26 +0100

    updating reanalaysis pipeline

--- src/main/java/uk/ac/ebi/ddi/service/db/repo/similarity/IReanalysisRepo.java
+++ src/main/java/uk/ac/ebi/ddi/service/db/repo/similarity/IReanalysisRepo.java
@@ -5,6 +5,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import uk.ac.ebi.ddi.service.db.model.similarity.ReanalysisData;
 
+import java.util.List;
+
 /**
  * Created by gaur on 27/07/17.
  */
@@ -13,5 +15,6 @@ public interface IReanalysisRepo extends MongoRepository<ReanalysisData,ObjectId
     @Query("{'$and': [{'accession': ?0},{'database': ?1}]}")
     ReanalysisData getReanalysisCount(String accession, String database);
 
-
+    @Query("{'similars.relationType':?0}")
+    List<ReanalysisData> getDatasetsByRelation(String relationValue);
 }
