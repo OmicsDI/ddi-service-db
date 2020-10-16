dee70d2aca7df1b0e24611f6b55a322616736769
Gaurhari <gaurharidass@gmail.com>
2020-10-16 12:57:38 +0100

    added attributes class for biostudies service

---
+++ src/main/java/uk/ac/ebi/ddi/service/db/model/biostudies/Attributes.java
@@ -0,0 +1,27 @@
+package uk.ac.ebi.ddi.service.db.model.biostudies;
+
+public class Attributes {
+
+    String name;
+    String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+
+
+
+}
