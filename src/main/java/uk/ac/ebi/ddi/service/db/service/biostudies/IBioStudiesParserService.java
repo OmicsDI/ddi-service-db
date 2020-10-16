f130ca9aea3a37f21b7c5fbad6ba2c0a585c69bc
Gaurhari <gaurharidass@gmail.com>
2020-10-16 13:05:32 +0100

    interface for biostudies

---
+++ src/main/java/uk/ac/ebi/ddi/service/db/service/biostudies/IBioStudiesParserService.java
@@ -0,0 +1,8 @@
+package uk.ac.ebi.ddi.service.db.service.biostudies;
+
+import java.io.IOException;
+
+public interface IBioStudiesParserService {
+
+    public void saveStudies() throws IOException;
+}
